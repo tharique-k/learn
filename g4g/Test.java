f725255ee5a1d4c8c0e62ef86bb20123c8f5549e
Tharique Shameem <developer.tharique@gmail.com>
2015-08-30 02:49:48 +0530

    file deleted

--- g4g/Test.java
+++ g4g/Test.java
@@ -4,9 +4,21 @@ public class Test {
 
 	
 	public static void main (String [] args){
+//		testSinglyLinkedList();
+		testBinaryTree();
 		
-		BinaryTree newTree = new BinaryTree();
-		newTree.populate();
+	}
+	
+	public static void testSinglyLinkedList(){
+		SinglyLinkedList sls = new SinglyLinkedList();
+		
+		sls.populate();
+		sls.print();
+		sls.reverse();
+		sls.print();
+	}
+	public static void testBinaryTree(){
+		MyBinaryTree newTree = MyBinaryTree.createBinaryTree();
 		newTree.fullInorder();
 		if (newTree.hasPathSum(newTree.getRoot(), 15)){
 			System.out.println("true");
